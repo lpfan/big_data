6bb89fbbec74283a799595e04a61975cbff7ec7d
rudy <misha.shchetinin@gmail.com>
2015-11-16 15:41:20 +0200

    modify streamer

--- tweetray/twitter_streaming.py
+++ tweetray/twitter_streaming.py
@@ -1,3 +1,6 @@
+import json
+import sys
+
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -8,11 +11,19 @@ import config
 class StdOutLIstener(StreamListener):
 
     def on_data(self, data):
-        print data
+        print json.loads(data)
+        with open('tweets.json', 'a') as file:
+            file.write(data)
         return True
 
     def on_error(self, status):
         print status
+        return True # Don't kill the stream
+
+    def on_timeout(self):
+        print >> sys.stderr, 'Timeout...'
+        return True # Don't kill the stream
+        print "Stream restarted"
 
 
 if __name__ == '__main__':
@@ -20,5 +31,9 @@ if __name__ == '__main__':
     auth = OAuthHandler(config.TWITTER_CONSUMER_KEY,
                         config.TWITTER_CONSUMER_SECRET)
     auth.set_access_token(config.TWITTER_ACCESS_TOKEN, config.TWITTER_ACCESS_SECRET)
-    stream = Stream(auth, listener)
-    stream.filter(track=['bwm', 'audi'])
+    while True:
+        try:
+            stream = Stream(auth, listener)
+            stream.filter(track=['buzzfeed'])
+        except:
+            continue
