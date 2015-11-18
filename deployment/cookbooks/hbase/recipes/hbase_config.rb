c41fdf251d8e42800d95998a8dd0705f32cd45f5
rudy <misha.shchetinin@gmail.com>
2015-11-18 19:10:44 +0200

    added configuration for hbase

--- deployment/cookbooks/hbase/recipes/hbase_config.rb
+++ deployment/cookbooks/hbase/recipes/hbase_config.rb
@@ -5,4 +5,20 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 
-include_recipe "hbase::hbase_install"
+#include_recipe "hbase::hbase_install"
+
+
+template '/home/hduser/.bashrc' do
+    source 'default/hduser_bashrc.erb'
+    owner 'hduser'
+    group 'hduser'
+end
+
+execute 'source user profile' do
+    user 'hduser'
+    command 'source ~/.bashrc'
+end
+
+template '/usr/local/hbase/conf/hbase-site.xml' do
+    source 'default/hbase-site.xml.erb'
+end
