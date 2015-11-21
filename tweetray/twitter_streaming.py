49d4e03f16f3bfd83610c3a6570cec4663931d03
rudy <misha.shchetinin@gmail.com>
2015-11-22 00:30:03 +0200

    first attempt to store data into hbase

--- tweetray/twitter_streaming.py
+++ tweetray/twitter_streaming.py
@@ -1,6 +1,8 @@
+import json
 import sys
 import time
 
+import happybase
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -10,8 +12,17 @@ import config
 
 class TweerayListener(StreamListener):
 
+    def __init__(self, api=None, tweets_table=None):
+        super(TweerayListener, self).__init__(api=api)
+        self.tweets_table = tweets_table
+
     def on_data(self, data):
-        print data
+        tweet = json.loads(data)
+        self.tweets_table.put(tweet['id'], {
+            'cf:text': tweet['text'],
+            'cf:user_name': tweet['user']['name'],
+            'cf:user_location': tweet['user']['location']
+        })
 
     def on_error(self, status):
         print status
@@ -24,8 +35,10 @@ class TweerayListener(StreamListener):
 
 
 def main():
-    track = ['buzzfeed']
-    listener = TweerayListener()
+    track = ['buzzfeed','cnnpolitics', 'hromadsketv', 'espresotv']
+    hbase_conn = happybase.Connection('localhost')
+    tweets_table = hbase_conn.table('tweets')
+    listener = TweerayListener(tweets_table=tweets_table)
     auth = OAuthHandler(config.TWITTER_CONSUMER_KEY,
                         config.TWITTER_CONSUMER_SECRET)
     auth.set_access_token(config.TWITTER_ACCESS_TOKEN, config.TWITTER_ACCESS_SECRET)
