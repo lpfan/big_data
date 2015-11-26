06105bb5b0d44a1c73f3e19b6007e8282a953c04
rudy <misha.shchetinin@gmail.com>
2015-11-26 22:40:39 +0200

    fix worker should work

--- tweetray/worker.py
+++ tweetray/worker.py
@@ -1,5 +1,6 @@
 import json
 import multiprocessing
+import os
 
 import happybase
 import pika
@@ -7,38 +8,33 @@ import pika
 import config
 
 
-RECIEVED_TWEET_COUNT = 0
-
 def tweet_collector(ch, method, properties, tweet_file):
     hbase_conn = happybase.Connection('localhost')
     tweets_table = hbase_conn.table('tweets')
-    print 'start collector'
-    print tweet_file
-
-    # with(tweet_file, 'r') as file:
-    #     tweets = file.readlines()
-
-    #for line in tweets:
-    #    tweet = json.loads(line)
-    #    text = tweet.get('text', '')
-    #    row_key = tweet.get('id_str', '')
-    #    user = tweet.get('user')
-    #    user_name = ''
-    #    location = ''
-    #    if user is not None:
-    #        user_name = user['name']
-    #        location = user.get('user_location', '')
-    #    try:
-    #        tweets_table.put(row_key, {
-    #            'cf:text': text,
-    #            'cf:user_name': user_name,
-    #            'cf:user_location': location,
-    #            'cf:timestam_ms': tweet['timestamp_ms']
-    #        })
-    #        print "[x] tweet %s stored" % row_key
-    #    except Exception:
-    #        pass
-
+    f = open(tweet_file, 'r', encoding='utf-8')
+    tweets = f.readlines()
+    f.close()
+    for line in tweets:
+        tweet = json.loads(line)
+        text = tweet.get('text', '')
+        row_key = tweet.get('id_str', '')
+        user = tweet.get('user')
+        user_name = ''
+        location = ''
+        if user is not None:
+            user_name = user['name']
+            location = user.get('user_location', '')
+        try:
+            tweets_table.put(row_key, {
+                'cf:text': text,
+                'cf:user_name': user_name,
+                'cf:user_location': location,
+                'cf:timestam_ms': tweet['timestamp_ms']
+            })
+            print "[x] tweet %s stored" % row_key
+        except Exception:
+            pass
+    os.remove(tweet_file)
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 def consume():
