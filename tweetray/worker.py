91dc6f1fde16613b0357575c973a6b6d40028cb4
rudy <misha.shchetinin@gmail.com>
2015-12-03 18:16:59 +0200

    fix worker.working now

--- tweetray/worker.py
+++ tweetray/worker.py
@@ -13,10 +13,10 @@ import config
 
 logging.basicConfig(filename=os.path.join(config.LOGS_PATH, 'workers.log'), level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-client = MongoClient(config.MONGO_HOST, config.MONGO_PORT)
 
 
 def tweet_collector(ch, method, properties, tweet_file):
+    client = MongoClient(config.MONGO_HOST, config.MONGO_PORT)
     try:
         f = codecs.open(tweet_file, 'r', encoding='utf-8')
         tweets = f.readlines()
@@ -32,22 +32,24 @@ def tweet_collector(ch, method, properties, tweet_file):
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 def consume():
+    print('prepear consuming')
     rabbit_mq_conn = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     rabbit_mq_channel = rabbit_mq_conn.channel()
     rabbit_mq_channel.queue_declare(queue=config.TWEET_QUEUE_NAME)
     rabbit_mq_channel.basic_consume(tweet_collector,
                                     queue=config.TWEET_QUEUE_NAME)
+    print('ready for consuming')
     try:
         rabbit_mq_channel.start_consuming()
     except KeyboardInterrupt:
         pass
 
-if __name__ == '__main__':
-
-    with multiprocessing.Pool(processes=config.WORKER_COUNT) as worker_pool:
-        try:
-            worker_pool.apply_async(consume)
-        except KeyboardInterrupt:
-            print(' [*] Exiting...')
-            worker_pool.terminate()
-            worker_pool.join()
+with multiprocessing.Pool(processes=config.WORKER_COUNT) as worker_pool:
+    worker_pool.apply_async(consume)
+    try:
+        while True:
+            continue
+    except KeyboardInterrupt:
+        print(' [*] Exiting...')
+        worker_pool.terminate()
+        worker_pool.join()
